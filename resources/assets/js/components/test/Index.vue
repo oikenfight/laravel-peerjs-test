bf566c92d2e5c3a6cdd2a4b17f294878b06909d2
YutaOikawa <oikenfight@gmail.com>
2018-07-25 22:32:28 +0900

    create test components.

---
+++ resources/assets/js/components/test/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item">
+                <router-link to="/test/receiver">Receiver</router-link>
+            </li>
+            <li class="list-group-item">
+                <router-link to="/test/sender">Sender</router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        },
+        created () {
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
